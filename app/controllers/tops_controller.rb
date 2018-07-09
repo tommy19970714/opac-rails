c2b55c438def7575ec19dcca7fd564662b655866
Toshiki Tomihira <toshiki0714@icloud.com>
2018-07-09 14:56:02 +0900

    add ransack

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,9 +1,14 @@
 class TopsController < ApplicationController
   def search
+    # @books = Book.where('tr LIKE ?', '%'+@word+'%')
+    @q = Book.search(params[:q])
+    @books = @q.result(distinct: true)
   end
 
   def list
-    @word = params[:word]
-    @books = Book.where('tr LIKE ?', '%'+@word+'%')
+    @word = params[:q]
+    # @books = Book.where('tr LIKE ?', '%'+@word+'%')
+    @q = Book.ransack(@word)
+    @books = @q.result(distinct: true)
   end
 end
